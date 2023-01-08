2b1fa8223316c31b140ee222b8cab73f4330793f
Han Chuen Foong <47974238+HanChuenFoong@users.noreply.github.com>
2023-01-08 01:03:38 -0800

    Create SimpleStorage.sol

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNnumber;
+
+    struct People {
+        uint favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber; 
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNnumber[_name] = _favoriteNumber;
+    }
+}
+// 0xd9145CCE52D386f254917e481eB44e9943F39138
+
+
